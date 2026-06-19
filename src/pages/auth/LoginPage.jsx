5421e174d9e1788d88a17f88c48298b359356cad
min011128 <min011128g@gmail.com>
2026-06-19 12:40:25 +0900

    20260619 소셜로그인 로고 추가

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -182,13 +182,33 @@ const LoginPage = () => {
           <div style={{ flex: 1, height: 1, background: "#e5e7eb" }} />
         </div>
 
-        <a href={KAKAO_AUTH_URL} className="btn-kakao">
+        {/* 카카오 로그인 */}
+        <a href={KAKAO_AUTH_URL} className="btn-kakao" style={{ gap: "8px" }}>
+          {/* 카카오 말풍선 로고 */}
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 3c-5.523 0-10 3.582-10 8 0 2.933 1.954 5.494 4.908 6.845l-1.241 4.545c-.137.502.408.924.845.633l5.352-3.565c.045.002.09.007.136.007 5.523 0 10-3.582 10-8s-4.477-8-10-8z"/>
+          </svg>
           카카오 로그인
         </a>
+
+        {/* 네이버 로그인 */}
         <a href={NAVER_AUTH_URL} className="btn-naver">
+          {/* 네이버 N 로고 */}
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M14.2 0H24v24h-9.8L9.8 12.4V24H0V0h9.8L14.2 11.6V0z"/>
+          </svg>
           네이버 로그인
         </a>
+
+        {/* 구글 로그인 */}
         <a href={GOOGLE_AUTH_URL} className="btn-google">
+          {/* 구글 G 로고 */}
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M23.745 12.27c0-.7-.06-1.4-.19-2.07H12v3.92h6.61c-.29 1.53-1.14 2.82-2.4 3.68v3.05h3.88c2.27-2.09 3.65-5.17 3.65-8.58z" fill="#4285F4"/>
+            <path d="M12 24c3.24 0 5.95-1.08 7.93-2.91l-3.88-3.05c-1.08.72-2.45 1.16-4.05 1.16-3.11 0-5.74-2.11-6.68-4.96H1.21v3.15C3.18 21.88 7.31 24 12 24z" fill="#34A853"/>
+            <path d="M5.32 14.24A7.16 7.16 0 0 1 4.91 12c0-.79.13-1.57.38-2.31V6.54H1.21A11.94 11.94 0 0 0 0 12c0 1.92.45 3.74 1.21 5.46l4.11-3.22z" fill="#FBBC05"/>
+            <path d="M12 4.75c1.77 0 3.35.61 4.6 1.8l3.42-3.42C17.95 1.19 15.24 0 12 0 7.31 0 3.18 2.12 1.21 5.46l4.11 3.22c.94-2.85 3.57-4.93 6.68-4.93z" fill="#EA4335"/>
+          </svg>
           구글 로그인
         </a>
 
